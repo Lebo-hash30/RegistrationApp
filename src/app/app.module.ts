b5f36994302f50853af333c6fb44ac56fce6059f
Lebo-hash30 <66554460+Lebo-hash30@users.noreply.github.com>
2020-08-05 09:50:13 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { Registration } from './registration'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,7 +11,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    Registration
   ],
   providers: [],
   bootstrap: [AppComponent]
